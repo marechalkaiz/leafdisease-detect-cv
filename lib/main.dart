f9125de27d3ccc1e7e221b2b9a648636537c4976
Maréchal Kaiz <kaisarcomics@gmail.com>
2024-12-27 18:10:30 +0700

    In progress

---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+// import 'dart:io';
+import 'take_picture.dart';
+import 'package:camera/camera.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // final cameras = await availableCameras();
+  // final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData.dark(),
+      // home: TakePictureScreen(camera: firstCamera),
+      home: HomePage(),
+    ),
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  // final CameraDescription camera;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Plant Leaf Disease Detector'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () async {
+                  final cameras = await availableCameras();
+                  final camera = cameras.first;
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              TakePictureScreen(camera: camera)));
+                },
+                child: Text('Start Detecting')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// class TakePictureScreen extends StatefulWidget {
+//   const TakePictureScreen({
+//     super.key,
+//     required this.camera,
+//   });
+
+//   final CameraDescription camera;
+
+//   @override
+//   TakePictureScreenState createState() => TakePictureScreenState();
+// }
+
+// class TakePictureScreenState extends State<TakePictureScreen> {
+//   late CameraController _controller;
+//   late Future<void> _initializeControllerFuture;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     _controller = CameraController(widget.camera, ResolutionPreset.medium);
+//     _initializeControllerFuture = _controller.initialize();
+//   }
+
+//   @override
+//   void dispose() {
+//     _controller.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Take a picture'),
+//       ),
+//       body: FutureBuilder<void>(
+//           future: _initializeControllerFuture,
+//           builder: (context, snapshot) {
+//             if (snapshot.connectionState == ConnectionState.done) {
+//               return CameraPreview(_controller);
+//             } else {
+//               return const Center(
+//                 child: CircularProgressIndicator(),
+//               );
+//             }
+//           }),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () async {
+//           try {
+//             await _initializeControllerFuture;
+//             final image = await _controller.takePicture();
+//             if (!context.mounted) return;
+
+//             await Navigator.of(context).push(
+//               MaterialPageRoute(
+//                 builder: (context) => DisplayPictureScreen(
+//                   imagePath: image.path,
+//                 ),
+//               ),
+//             );
+//           } catch (e) {
+//             print(e);
+//           }
+//         },
+//         child: const Icon(Icons.camera_alt),
+//       ),
+//     );
+//   }
+// }
+
+// class DisplayPictureScreen extends StatelessWidget {
+//   final String imagePath;
+
+//   const DisplayPictureScreen({super.key, required this.imagePath});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: const Text('Display the picture')),
+//       body: Image.file(File(imagePath)),
+//     );
+//   }
+// }
